019ac9f8836b5c2a3a1c7bf6afff36c7be8c7e6c
Tsai0409 <138881929+Tsai0409@users.noreply.github.com>
2025-07-15 22:01:54 +0800

    Update yolo_configs.py

--- src/yolo_configs.py
+++ src/yolo_configs.py
@@ -6,6 +6,12 @@ import os
 from pdb import set_trace as st
 import copy
 
+# kaggle input
+DATA_KAGGLE_DIR = "/kaggle/input/rsna-2024-lumbar-spine-degenerative-classification"
+
+# 設定環境變數
+WORKING_DIR="/kaggle/working/duplicate"
+
 class Baseline:
     def __init__(self):
         self.compe = 'rsna'
@@ -41,7 +47,9 @@ class Baseline:
         self.no_aug_epochs = 15
         self.warmup_epochs = 5
         self.nmsthre = 0.45
-        self.absolute_path = os.getcwd()
+        # self.absolute_path = os.getcwd()
+        self.absolute_path = '/kaggle/working/duplicate'
+
 ### required columns ###
 ## train
 # path
@@ -66,11 +74,13 @@ class rsna_axial_all_images_left_yolox_x(Baseline):
         self.update_json = False
         self.no_aug_epochs = 15
         self.warmup_epochs = 5
-        self.pretrained_path = '/groups/gca50041/ariyasu/yolox_weights/yolox_x.pth'
+        # self.pretrained_path = '/groups/gca50041/ariyasu/yolox_weights/yolox_x.pth'
+        self.pretrained_path = '/kaggle/input/pretrain-7/yolox_x.pth'
         self.image_size = (512, 512)
         self.batch_size = 8
         self.predict_valid = True
-        self.train_df_path = 'input/train_axial_for_yolo_all_image_v1.csv'
+        # self.train_df_path = 'input/train_axial_for_yolo_all_image_v1.csv'
+        self.train_df_path = f'{WORKING_DIR}/csv_train/region_estimation_by_yolox_6/train_axial_for_yolo_all_image_v1.csv'
         self.train_df = pd.read_csv(self.train_df_path)
         self.train_df = self.train_df[self.train_df.condition == 'Left Subarticular Stenosis']
         self.train_df['class_id'] = 0
@@ -80,11 +90,13 @@ class rsna_axial_all_images_left_yolox_x(Baseline):
         val = self.train_df[self.train_df.fold==0]
         self.train_df['fold'] = -1
         self.train_df = pd.concat([self.train_df, val])
-        self.test_df_path = 'input/train_with_fold.csv'
+        # self.test_df_path = 'input/train_with_fold.csv'
+        # self.test_df_path = f'{WORKING_DIR}/csv_train/preprocess_4/train_with_fold.csv'
         self.test_df = pd.read_csv(self.test_df_path)
         self.test_df = self.test_df[self.test_df.series_description=='Axial T2']
         self.test_df['instance_number'] = self.test_df.path.apply(lambda x: int(x.split('___')[-1].replace('.png', '')))
-        ldf = pd.read_csv('input/axial_closest_df.csv')
+        # ldf = pd.read_csv('input/axial_closest_df.csv')
+        ldf = pd.read_csv(f'{WORKING_DIR}/csv_train/axial_level_estimation_2/axial_closest_df.csv')
         ldf = ldf[ldf.closest==1]
         ldf = ldf[ldf.dis<3]
         ldf['pred_level'] = ldf.level.values
@@ -101,7 +113,8 @@ class rsna_axial_all_images_right_yolox_x(Baseline):
         self.image_size = (512, 512)
         self.batch_size = 8
         self.predict_valid = True
-        self.train_df_path = 'input/train_axial_for_yolo_all_image_v1.csv'
+        # self.train_df_path = 'input/train_axial_for_yolo_all_image_v1.csv'
+        self.train_df_path = f'{WORKING_DIR}/csv_train/region_estimation_by_yolox_6/train_axial_for_yolo_all_image_v1.csv'
         self.train_df = pd.read_csv(self.train_df_path)
         self.train_df = self.train_df[self.train_df.condition != 'Left Subarticular Stenosis']
         self.train_df['class_id'] = 0
@@ -111,11 +124,13 @@ class rsna_axial_all_images_right_yolox_x(Baseline):
         val = self.train_df[self.train_df.fold==0]
         self.train_df['fold'] = -1
         self.train_df = pd.concat([self.train_df, val])
-        self.test_df_path = 'input/train_with_fold.csv'
+        # self.test_df_path = 'input/train_with_fold.csv'
+        self.test_df_path = f'{WORKING_DIR}/csv_train/preprocess_4/train_with_fold.csv'
         self.test_df = pd.read_csv(self.test_df_path)
         self.test_df = self.test_df[self.test_df.series_description=='Axial T2']
         self.test_df['instance_number'] = self.test_df.path.apply(lambda x: int(x.split('___')[-1].replace('.png', '')))
-        ldf = pd.read_csv('input/axial_closest_df.csv')
+        # ldf = pd.read_csv('input/axial_closest_df.csv')
+        ldf = pd.read_csv(f'{WORKING_DIR}/csv_train/axial_level_estimation_2/axial_closest_df.csv')
         ldf = ldf[ldf.closest==1]
         ldf = ldf[ldf.dis<3]
         ldf['pred_level'] = ldf.level.values
