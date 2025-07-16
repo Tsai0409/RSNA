392d0cdd4a4ec6f3102b10e535955b815ff26865
Tsai0409 <138881929+Tsai0409@users.noreply.github.com>
2025-07-16 22:41:11 +0800

    Update yolo_configs.py

--- src/yolo_configs.py
+++ src/yolo_configs.py
@@ -87,8 +87,8 @@ class rsna_axial_all_images_left_yolox_x(Baseline):
         self.train_df['class_id'] = 0
         self.train_df['class_name'] = 'left'
         self.predict_test = True
-        self.epochs = 40
-        # self.epochs = 3
+        # self.epochs = 40
+        self.epochs = 2
         val = self.train_df[self.train_df.fold==0]
         self.train_df['fold'] = -1
         self.train_df = pd.concat([self.train_df, val])
