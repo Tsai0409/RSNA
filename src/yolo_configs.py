1db982cf3f467acfabb39f8a70ea2654d40a2064
Tsai0409 <138881929+Tsai0409@users.noreply.github.com>
2025-07-16 20:29:03 +0800

    Update yolo_configs.py

--- src/yolo_configs.py
+++ src/yolo_configs.py
@@ -87,8 +87,8 @@ class rsna_axial_all_images_left_yolox_x(Baseline):
         self.train_df['class_id'] = 0
         self.train_df['class_name'] = 'left'
         self.predict_test = True
-        # self.epochs = 40
-        self.epochs = 3
+        self.epochs = 40
+        # self.epochs = 3
         val = self.train_df[self.train_df.fold==0]
         self.train_df['fold'] = -1
         self.train_df = pd.concat([self.train_df, val])
@@ -118,6 +118,7 @@ class rsna_axial_all_images_right_yolox_x(Baseline):
         self.predict_valid = True
         # self.train_df_path = 'input/train_axial_for_yolo_all_image_v1.csv'
         self.train_df_path = f'{WORKING_DIR}/csv_train/region_estimation_by_yolox_6/train_axial_for_yolo_all_image_v1.csv'
+        # self.train_df_path = f'{WORKING_DIR}/csv_train/region_estimation_by_yolox_holdout_6/train_axial_for_yolo_all_image_v1.csv'
         self.train_df = pd.read_csv(self.train_df_path)
         self.train_df = self.train_df[self.train_df.condition != 'Left Subarticular Stenosis']
         self.train_df['class_id'] = 0
@@ -129,7 +130,8 @@ class rsna_axial_all_images_right_yolox_x(Baseline):
         self.train_df['fold'] = -1
         self.train_df = pd.concat([self.train_df, val])
         # self.test_df_path = 'input/train_with_fold.csv'
-        self.test_df_path = f'{WORKING_DIR}/csv_train/preprocess_4/train_with_fold.csv'
+        # self.test_df_path = f'{WORKING_DIR}/csv_train/preprocess_4/train_with_fold.csv'
+        self.test_df_path = f'{WORKING_DIR}/csv_train/preprocess_holdout_4/train_with_fold_holdout_test.csv'
         self.test_df = pd.read_csv(self.test_df_path)
         self.test_df = self.test_df[self.test_df.series_description_y=='Axial T2']
         self.test_df['instance_number'] = self.test_df.path.apply(lambda x: int(x.split('___')[-1].replace('.png', '')))
