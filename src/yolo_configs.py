31bab9cbe7c37306befd6a2f7f89de631a1351f8
Tsai0409 <138881929+Tsai0409@users.noreply.github.com>
2025-07-21 11:55:26 +0800

    0721

--- src/yolo_configs.py
+++ src/yolo_configs.py
@@ -89,7 +89,7 @@ class rsna_axial_all_images_left_yolox_x(Baseline):
         self.predict_test = True
         self.epochs = 40
         # self.epochs = 2
-        val = self.train_df[self.train_df.fold==0]
+        val = self.train_df[self.train_df.fold==1]
         self.train_df['fold'] = -1
         self.train_df = pd.concat([self.train_df, val])
         # self.test_df_path = 'input/train_with_fold.csv'
@@ -122,12 +122,12 @@ class rsna_axial_all_images_right_yolox_x(Baseline):
         self.train_df_path = f'{WORKING_DIR}/csv_train/region_estimation_by_yolox_holdout_6/train_axial_for_yolo_all_image_v1.csv'
         self.train_df = pd.read_csv(self.train_df_path)
         self.train_df = self.train_df[self.train_df.condition != 'Left Subarticular Stenosis']
-        self.train_df['class_id'] = 0
+        self.train_df['class_id'] = 1
         self.train_df['class_name'] = 'right'
         self.predict_test = True
         self.epochs = 40
         # self.epochs = 3
-        val = self.train_df[self.train_df.fold==0]
+        val = self.train_df[self.train_df.fold==1]
         self.train_df['fold'] = -1
         self.train_df = pd.concat([self.train_df, val])
         # self.test_df_path = 'input/train_with_fold.csv'
