8f8fa6147aea543055eb6e09276f917b92b06f18
Tsai0409 <138881929+Tsai0409@users.noreply.github.com>
2025-07-29 18:51:43 +0800

    0729

--- predict.py
+++ predict.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
     args = parse_args()
 
     if args.type == 'classification':  # default='classification'
-        from src_new.configs import *
+        from src.configs import *
     elif args.type == 'effdet':
         from src.effdet_configs import *
     elif args.type == 'seg':
