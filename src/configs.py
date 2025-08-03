fc5afe9a945e7a8297454003c26d3925af70b9eb
Tsai0409 <138881929+Tsai0409@users.noreply.github.com>
2025-08-03 14:33:46 +0800

    Update configs.py

--- src/configs.py
+++ src/configs.py
@@ -1166,7 +1166,6 @@ class rsna_saggital_spinal_crop_base(rsna_v1):
         self.drop_path_rate = 0.0
         base_model = timm.create_model(self.model_name, pretrained=True, num_classes=1, drop_rate=self.drop_rate, drop_path_rate=self.drop_path_rate)
         self.model = RSNA2ndModel(base_model=base_model, num_classes=len(self.label_features), pool='avg', swin=False)
-        # self.model = timm.create_model(self.model_name, pretrained=True, num_classes=self.num_classes, drop_rate=self.drop_rate, drop_path_rate=self.drop_path_rate)
 
         self.metric = None
         self.memo = ''
@@ -1374,7 +1373,7 @@ class rsna_saggital_mil_ss_crop_base(rsna_v1):
 
         base_model = timm.create_model(self.model_name, pretrained=True, num_classes=1, drop_rate=self.drop_rate, drop_path_rate=self.drop_path_rate)
         self.model = RSNA2ndModel(base_model=base_model, num_classes=len(self.label_features), pool='avg', swin=False)
-        # self.model = RSNA2ndModel(num_classes=len(self.label_features), pool='avg', swin=False)
+        
         self.box_crop = True
         self.box_crop_x_ratio = 0.4
         self.box_crop_y_ratio = 0.2
@@ -1811,7 +1810,7 @@ class rsna_saggital_mil_nfn_crop_base(rsna_v1):
 
         base_model = timm.create_model(self.model_name, pretrained=True, num_classes=1, drop_rate=self.drop_rate, drop_path_rate=self.drop_path_rate)
         self.model = RSNA2ndModel(base_model=base_model, num_classes=len(self.label_features), pool='avg', swin=False)
-        # self.model = RSNA2ndModel(num_classes=len(self.label_features), pool='avg', swin=False)
+
         self.box_crop = True
         self.box_crop_x_ratio = 0.4
         self.box_crop_y_ratio = 0.2
