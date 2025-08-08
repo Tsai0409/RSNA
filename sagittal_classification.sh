57f448e4cac587ba89c9663241af1b8b0046a943
Tsai0409 <138881929+Tsai0409@users.noreply.github.com>
2025-08-08 14:30:24 +0800

    Update sagittal_classification.sh

--- sagittal_classification.sh
+++ sagittal_classification.sh
@@ -17,16 +17,16 @@ PREDICT_SCRIPT="$WORKING_DIR/predict.py"
 
 # 設置 configs 和 folds 變數
 configs=(
-    # "rsna_saggital_mil_spinal_crop_x03_y05" 
-    # "rsna_saggital_mil_spinal_crop_x03_y07" 
+    "rsna_saggital_mil_spinal_crop_x03_y05" 
+    "rsna_saggital_mil_spinal_crop_x03_y07" 
 
-    # "rsna_saggital_mil_ss_crop_x03_y05_96" 
-    # "rsna_saggital_mil_ss_crop_x03_y07_96" 
-    # "rsna_saggital_mil_ss_crop_x03_y2_96" 
-    # "rsna_saggital_mil_ss_crop_x1_y07_96" 
+    "rsna_saggital_mil_ss_crop_x03_y05_96" 
+    "rsna_saggital_mil_ss_crop_x03_y07_96" 
+    "rsna_saggital_mil_ss_crop_x03_y2_96" 
+    "rsna_saggital_mil_ss_crop_x1_y07_96" 
     
-    # "rsna_saggital_mil_nfn_crop_x07_y1_v2" 
-    # "rsna_saggital_mil_nfn_crop_x15_y1_v2" 
+    "rsna_saggital_mil_nfn_crop_x07_y1_v2" 
+    "rsna_saggital_mil_nfn_crop_x15_y1_v2" 
     "rsna_saggital_mil_nfn_crop_x03_y1_v2" 
     "rsna_saggital_mil_nfn_crop_x05_y05_v2"
 )
@@ -39,20 +39,20 @@ do
     for fold in "${folds[@]}"
     do
         # 執行訓練腳本
-        cmd="python $TRAIN_SCRIPT -c $config -f $fold"
-        echo "Executing: $cmd"
-        if ! eval $cmd; then
-            echo "Error: Training failed for config $config fold $fold."
-            continue  # 跳過失敗的配置，繼續執行其他
+        # cmd="python $TRAIN_SCRIPT -c $config -f $fold"
+        # echo "Executing: $cmd"
+        # if ! eval $cmd; then
+        #     echo "Error: Training failed for config $config fold $fold."
+        #     continue  # 跳過失敗的配置，繼續執行其他
         fi
 
         # 執行預測腳本
-        # infcmd="python $PREDICT_SCRIPT -c $config -f $fold"
-        # echo "Executing: $infcmd"
-        # if ! eval $infcmd; then
-        #     echo "Error: Prediction failed for config $config fold $fold."
-        #     continue  # 跳過失敗的配置，繼續執行其他
-        # fi
+        infcmd="python $PREDICT_SCRIPT -c $config -f $fold"
+        echo "Executing: $infcmd"
+        if ! eval $infcmd; then
+            echo "Error: Prediction failed for config $config fold $fold."
+            continue  # 跳過失敗的配置，繼續執行其他
+        fi
 
         echo "----------------------------------------"
     done
