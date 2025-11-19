cd940da8a3436fc5c596300bda386d1ce99676db
Tsai0409 <138881929+Tsai0409@users.noreply.github.com>
2025-11-19 20:30:41 +0800

    Update configs.py

--- src/configs.py
+++ src/configs.py
@@ -744,9 +744,8 @@ class MultiClassFocalLoss(nn.Module):
         else:
             self.alpha = None
 
-        print(">>> USING FOCAL:", id(self), "alpha:", self.alpha.device, "targets:", targets.device)
-
     def forward(self, logits, targets):
+        print(">>> USING FOCAL:", id(self), "alpha:", self.alpha.device, "targets:", targets.device)
         ce_loss = F.cross_entropy(logits, targets, reduction='none')
         pt = torch.exp(-ce_loss)
 
