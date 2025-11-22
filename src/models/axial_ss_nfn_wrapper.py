e8556d08de93bb0ddffdda98c3ef1a02396c5243
Tsai0409 <138881929+Tsai0409@users.noreply.github.com>
2025-11-22 22:33:46 +0800

    Update axial_ss_nfn_wrapper.py

--- src/models/axial_ss_nfn_wrapper.py
+++ src/models/axial_ss_nfn_wrapper.py
@@ -12,6 +12,7 @@ from src.lightning.lightning_modules.classification import MyLightningModule
 class AxialSSNFNWrapper(MyLightningModule):
     def __init__(self, base_model, lr, criterion_nfn, criterion_ss):
         super().__init__(cfg)
+        cfg=self,
         self.base_model = base_model
         self.lr = lr
         self.criterion_nfn = criterion_nfn
